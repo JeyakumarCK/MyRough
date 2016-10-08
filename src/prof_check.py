99b190c518718a2430521eca3adc01bb69177f2f
JeyakumarCK <Jeyakumar.ck@gmail.com>
2016-10-08 23:21:54 +0530

    further update

--- src/prof_check.py
+++ src/prof_check.py
@@ -1,8 +1,22 @@
+import urllib
+
 def read_txt():
     quotefile = open("C:\JK\Udacity\Python\movie_quotes.txt")
     quotetext = quotefile.read()
     print quotetext
     quotefile.close()
+    check_profanity(quotetext)
     #End of Function
+    
+def check_profanity(test_text):
+    conn = urllib.urlopen('http://www.wdylike.appspot.com/?q='+test_text)
+    res = conn.read()
+    print res
+    if "true" in res:
+        print "Profanity Alert: There is a curse word"
+    else:
+        print "You are all good"
+    conn.close()
+    #End of function
     
 read_txt()
